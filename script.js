fed2723e917f6c4681ebc11bf654face8b4825cd
Shamimapoly <shamimapoly365@gmail.com>
2025-07-10 22:20:35 +0800

    update

---
+++ script.js
@@ -0,0 +1,111 @@
+// Veriable :
+
+// var
+// modify kora jabe
+// let
+// modify kora jabe
+var a = 100;
+let b = 200;
+b=500;
+// const (Constant)
+const c = 300;
+// console.log(b);
+
+// data type :
+// 1) String
+// let neew = "hhhh";
+// console.log(neew);
+// console.log(typeof(neew));
+// 2) Number
+// let new2 = 30;
+// console.log(typeof(new2));
+// 3) bolean
+// let new3 = true;
+// console.log(typeof(new3));
+// 4)object
+// let student = {
+//     id : 1,
+//     name : "dd"
+// }
+
+// console.log(typeof(student));
+
+// Name array : (Object)
+// let new2 = [1,2,3,4]
+// console.log(typeof(new2));
+
+
+// null (Object)
+// let new4 = null;
+// console.log(new4);
+// console.log(typeof(new4));
+
+// undefined
+let gg;
+// gg=100
+// console.log(gg);
+console.log(typeof(gg));
+
+
+
+
+
+
+
+
+// templete :
+// let name1 = "r";
+// let name2 = `${name1} 100`;
+// console.log(name2);
+
+
+
+// function 
+// normal funtion:
+function greet(b,c){
+    // console.log(`b holo : ${b}  , c holo: ${c}`);
+    // console.log(b,c);
+    // +  - 
+    // console.log(`result = ${b%2} if 0 then  jor`);
+    
+    // if(b%2==0){
+    //     console.log('jor');
+        
+    // }else{
+    //     console.log('bijor');
+        
+    // }
+} 
+
+greet(610,60)
+// arrow funtion:
+// let greet2 = (a,b) =>{
+//     console.log(`Wellcome to ${a} and ${b}`);
+
+// }
+
+// greet2("lima","k")
+
+
+// condition :
+// let d = true;
+// console.log(d);
+
+
+// if(d == true){
+// console.log("Okk");
+
+// }else{
+//     console.log("not ok");
+    
+// }
+
+// let e=10;
+// let f=10;
+// if(e==f){
+//     console.log(e+f);
+    
+// }else{
+//     console.log('not match');
+    
+// }
